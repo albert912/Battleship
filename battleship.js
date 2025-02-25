de808dfebd3009c6a73eb64e9598db5fd74375bb
albert912 <aatunckol@gmail.com>
2025-02-25 19:46:18 +0300

    Create Gameboard class

--- battleship.js
+++ battleship.js
@@ -1,137 +1,112 @@
-class Ship {
+export class Ship {
     constructor(length) {
-      this.length = length;
-      this.hitNum = 0;
-      this.sunk = false;
+        this.length = length;
+        this.hits = new Array(length).fill(false);  
+        this.sunk = false;
     }
-  
-    hit() {
-      this.hitNum ++;
+
+    hit(index) {
+        if (!this.hits[index]) {
+            this.hits[index] = true;
+        }
     }
-  
+
     isSunk() {
-      if (this.length <= this.hitNum) {
-       /* this.sunk = true;*/
-       return true;
-      }
+        return this.hits.every(part => part === true); 
     }
-  }
+}
 
-class Gameboard {
+export class Gameboard {
+    constructor(rows, cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.gameboard = Array.from({ length: rows }, () => Array(cols).fill(null));
+        this.missed = [];
+        this.ships = [];
+    }
 
+    placeShip(ship, x, y, direction) {
+        if (direction === "horizontal") {
+            if (y + ship.length > this.cols) return false;
 
-    const rows = 3;
-    const columns = 3;
-    const gameboard = [];
+            for (let i = 0; i < ship.length; i++) {
+                if (this.gameboard[x][y + i] !== null) return false;
+            }
 
+            for (let i = 0; i < ship.length; i++) {
+                this.gameboard[x][y + i] = { ship: ship, partIndex: i };
+            }
+        } else if (direction === "vertical") {
+            if (x + ship.length > this.rows) return false;
 
+            for (let i = 0; i < ship.length; i++) {
+                if (this.gameboard[x + i][y] !== null) return false;
+            }
 
+            for (let i = 0; i < ship.length; i++) {
+                this.gameboard[x + i][y] = { ship: ship, partIndex: i };
+            }
+        }
 
+        this.ships.push(ship);
+        return true; 
+    }
 
+    receiveAttack(x, y) {
+        const cell = this.gameboard[x][y];
 
+        if (cell !== null && cell !== "miss") {
+            const { ship, partIndex } = cell;
 
+            if (ship.hits[partIndex]) {
+               
+            }
 
+            ship.hit(partIndex);
 
-    constructor() {
+            if (ship.isSunk()) {
+                console.log('The ship is sunk!');
+            }
+        } else if (cell === null) {
+            if (this.missed.some(miss => miss.x === x && miss.y === y)) {
+                return; 
+            }
+
+            this.gameboard[x][y] = "miss";  
+            this.missed.push({ x, y }); 
+        }
+    }
+
+    checkIfAllShipsSunk() {
+        const allSunk = this.ships.every(ship => ship.isSunk()); 
+
+        if (allSunk) {
+            console.log("All ships are sunk!");
+        }
+
+        return allSunk;
+    }
 }
 
 
 
-}
-  
 
-module.exports = Ship;
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*class Ship {
-    constructor(length, hit, sunk) {
-      this.length = length;
-      this.hit = hit;
-      this.sunk = sunk;
-    }
-
-
-
-    hit() {
-    return ${this.hit} ++ ; 
-    }
-
-    isSunk() {
-
-        // A ship is considered sunk if the number of hits are equal to its length.
-         if ( ${this.length} == ${this.hit} ) {
-           return true;
-         }
-
-         return false;
-      }
-
-  }
-  
-
-
-  const Battleship = new Ship(5,4, yes);
-
-
-  console.log(Battleship.isSunk()); 
-
-
-
-module.exports = Battleship;*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /*class Gameboard {
-    // Constructor method to initialize properties
-    constructor(parameter1, parameter2) {
-      this.property1 = parameter1;
-      this.property2 = parameter2;
+export class Player {
+    constructor(type) {
+      this.type = type;  
+      this.gameboard = new Gameboard(10, 10);  
     }
   
-    // Method within the class
-    methodName() {
-      console.log("This is a method");
-    }
-  }
-  
+}  
 
 
-  class Player {
-    // Constructor method to initialize properties
-    constructor(parameter1, parameter2) {
-      this.property1 = parameter1;
-      this.property2 = parameter2;
-    }
-  
-    // Method within the class
-    methodName() {
-      console.log("This is a method");
-    }
-  }*/
+
+const place = new Gameboard(10, 10);
+
+place.placeShip(new Ship(5), 5, 5, "vertical");
+
+module.exports = {
+    Ship,
+    Gameboard,
+    Player
+};
